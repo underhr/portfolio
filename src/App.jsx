59791c9d4828377225621d0d55388ee64f6b6ba9
underhr <haleyroseu@gmail.com>
2025-11-04 17:15:23 -0600

    added localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,33 @@ export default function App() {
   const [current, setCurrent] = useState("Home");
   const [isDark, setIsDark] = useState(false);
   const [theme, setTheme] = useState("green");
+  const [loaded, setLoaded] = useState(false);
 
   const toggleDark = () => setIsDark(prev => !prev);
+
+  useEffect(() => {
+    const savedCurrent = localStorage.getItem("current");
+    const savedIsDark = localStorage.getItem("isDark");
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedCurrent) setCurrent(savedCurrent);
+    if (savedIsDark !== null) setIsDark(savedIsDark === "true");
+    if (savedTheme) setTheme(savedTheme);
+
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (loaded) localStorage.setItem('current', current);
+  }, [current, loaded]);
+
+  useEffect(() => {
+    if (loaded) localStorage.setItem('isDark', isDark);
+  }, [isDark, loaded]);
+
+  useEffect(() => {
+    if (loaded) localStorage.setItem('theme', theme);
+  }, [theme, loaded]);
 
   return (
     <div data-theme={`${theme}${isDark ? "-dark" : ""}`} className="min-h-screen flex flex-col sm:flex-row bg-(--bg-lighter) text-(--text-lighter) transition-colors duration-300">
