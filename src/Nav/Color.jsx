2f1d3ae524103d823d7342e5998ae4477a10a626
underhr <haleyroseu@gmail.com>
2025-11-04 16:25:16 -0600

    Finished theme colors

--- src/Nav/Color.jsx
+++ src/Nav/Color.jsx
@@ -3,7 +3,7 @@ export default function Color({ color, setTheme }) {
     return(
         <div 
             data-theme={color} 
-            className="bg-(--text-darker) rounded-2xl h-8 w-8 cursor-pointer"
+            className="bg-linear-to-l from-(--bg-darker) to-(--text-darker) rounded-2xl h-8 w-8 cursor-pointer"
             onClick={() => setTheme(color)}>
         </div>
     );
--- src/Nav/ColorPalette.jsx
+++ src/Nav/ColorPalette.jsx
@@ -3,16 +3,18 @@ import Color from "./Color";
 
 export default function ColorPalette({ paletteOpen, setTheme }) {
     return(
-        <div className={`absolute top-12 right-6 sm:-top-12 sm:right-0 sm:left-15 bg-white h-30 w-30 rounded-2xl items-center justify-evenly flex-wrap ${paletteOpen ? 'flex' : 'hidden'}`}>
+        <div className={`border-2 border-(--text-lighter) bg-white absolute top-12 right-6 sm:-top-12 sm:right-0 sm:left-15 h-30 w-30 rounded-2xl items-center justify-evenly flex-wrap ${paletteOpen ? 'flex' : 'hidden'}`}>
+            <Color color="white" setTheme={setTheme}/>
+            <Color color="brown" setTheme={setTheme}/>
+            <Color color="pink" setTheme={setTheme}/>
+            
             <Color color="red" setTheme={setTheme}/>
-            <Color color="blue" setTheme={setTheme}/>
+            <Color color="orange" setTheme={setTheme}/>
+            <Color color="yellow" setTheme={setTheme}/>
+
             <Color color="green" setTheme={setTheme}/>
-            <Color color="violet" setTheme={setTheme}/>
             <Color color="blue" setTheme={setTheme}/>
-            <Color color="green" setTheme={setTheme}/>
-            <Color color="violet" setTheme={setTheme}/>
-            <Color color="blue" setTheme={setTheme}/>
-            <Color color="green" setTheme={setTheme}/>
+            <Color color="purple" setTheme={setTheme}/>
 
         </div>
     );
