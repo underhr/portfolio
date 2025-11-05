1c104737343c46b70616c83f5c3e59b8aa9a954a
underhr <haleyroseu@gmail.com>
2025-11-04 20:00:35 -0600

    added active state bg for nav buttons

--- src/Nav/NavItem.jsx
+++ src/Nav/NavItem.jsx
@@ -1,7 +1,7 @@
 
 export default function navItem({ name , setCurrent}) {
     return(
-        <li className='cursor-pointer hover:bg-(--bg-lighter) text-center rounded-2xl p-2'
+        <li className='cursor-pointer active:bg(--bg-lighter) hover:bg-(--bg-lighter) text-center rounded-2xl p-2'
         onClick={() => setCurrent(name)}>{name}</li>
     );
 }
